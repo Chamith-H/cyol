41d17a29107da0d9231c9802fd9db519e896eb82
Chamith-H <chamith.softwaredev@gmail.com>
2023-04-27 11:27:46 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Layout/Header';
 import Body from './Components/Layout/Body';
 import Footer from './Components/Layout/Footer';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.pageYOffset > 150) {
+            setScrolled(true);
+          } else {
+            setScrolled(false);
+          }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+    
+    }, []);
+
   return (
     <div className="App">
       <div className="Hero-Image">
-          <div className="Align-Hero"/>
+          <div className={scrolled?"Scrolled-Hero-Body":"Align-Hero"}/>
       </div>
 
       <div className="Header-Margin"/>
