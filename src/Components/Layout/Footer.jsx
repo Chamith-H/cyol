1c1fdb8b7a32c774161ffd9fcea7ebae17d834e4
Chamith-H <chamith.softwaredev@gmail.com>
2023-05-07 20:23:48 +0530

    done

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -6,16 +6,16 @@ const Footer =( props )=> {
         <div className="Footer">
             <div className="mx-lg-5 px-5">
                 <div className="row gx-4 gy-5">
-                    <div className="col-md-12 col-lg-3 px-md-5 px-lg-0 Footer-Brand d-flex justify-content-center align-items-center">
+                    <div className="col-md-12 col-lg-3 px-md-5 px-lg-0 Footer-Brand d-flex justify-content-center justify-content-lg-start align-items-center">
                         <img className="Footer-Logo mb-4" src={Logo} alt="" />
                     </div>
 
-                    <div className="col-md-7 Footer-Address">
+                    <div className="col-md-9 col-lg-7 Footer-Address">
                         <p><i class="bi bi-geo-alt-fill"></i>U102, 46 GRAINGERS ROAD, <br></br>WEST FOOTSCRAY VIC 3012</p>
                         <p><i class="bi bi-envelope-at-fill"></i>info@Digitustecglobal.com</p>
                     </div>
 
-                    <div className="col-md-4 col-lg-2 Navigation-Footer d-flex flex-column align-items-center align-items-md-end">
+                    <div className="col-md-3 col-lg-2 Navigation-Footer d-flex flex-column align-items-center align-items-md-end">
                         <h5>Quick Links</h5>
                         <p type="button" onClick={() => props.scroll_Wanted('services')}>Services</p>
                         <p type="button" onClick={() => props.scroll_Wanted('about')}>About us</p>
